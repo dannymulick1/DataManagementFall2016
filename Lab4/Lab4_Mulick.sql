4bdea6efe4710093ed58831f66dbe1fc1dec97c0
dannymulick1 <danny.mulick1@marist.edu>
2016-09-28 23:52:40 -0400

    Commit to fix issue with files

--- Lab4/Lab4_Mulick.sql
+++ Lab4/Lab4_Mulick.sql
@@ -1,4 +1,6 @@
-﻿-- query for question 1: get cities of agents booking for customer with c006
+﻿--Lab 4, coded by Danny Mulick in the Fall of 2016
+
+-- query for question 1: get cities of agents booking for customer with c006
 
 select city
   from agents
@@ -71,5 +73,36 @@ select *
                     );
 
 
+/*
+Danny Mulick
+Lab 4
+Database Management
+Fall 2016
 
+8. Tell me about check constraints: What are they? What are they good for? What’s the advantage of putting that sort of thing inside the database? Make up some examples of good uses of check constraints and some examples of bad uses of check constraints. Explain the differences in your examples and argue your case.
+	A.	A check constraint is a limiter that can be placed either on individual column or a table as a whole, which stops the user from inputting values that are against the rules that the programmer sets.
+	B.	The constraints are good for maintaining quality of data.
+	C.	The advantage of placing a constraint inside your database is that it helps to filter out poor quality data, which in turn makes analyses and synthesis of information easier.
+	D.	Good check constraints
+		a.	Making sure numbers are integers (Whole numbers)
+		b.	Making sure numbers are positive (greater than zero)
+		c.	Making sure an input isn’t over a maximum length
+	E.	Bad check constraints
+		a.	Requiring a user to put hyphens in their phone number
+		b.	The required data value is outside the provided set of values
+	F.	Explain the differences in your examples and argue your case
+		a.	Good constraints
+			i.	Integers
+				1.	Making sure your numbers are integers is a good constraint because it keeps your data consistent, and helps to reduce math errors from rounding.
+			ii.	Positive
+				1.	Ensuring that your numbers are positive helps with transactions, as you do not want someone removing a negative amount of money from an account.
+			iii.	Max Length
+				1.	This constraint helps to keep the size of your database down, as well as keeping the speed of logging in fast.
+		b.	Bad constraints
+			i.	Hyphens in a phone number
+				1.	This is a bad constraint because it requires the user to input a certain format that doesn’t always make sense when read
+			ii.	Required value out of range of values
+				1.	This is a bad constraint because it is asking the user to input a value that they cannot input
+
+*/
  
