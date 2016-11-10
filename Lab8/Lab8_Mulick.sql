124871c200a4221ca91c7948194ec6cc20d51e65
dannymulick1 <danny.mulick1@marist.edu>
2016-11-09 22:39:00 -0500

    Commit for lab 8 SQL file
    Committing like the title says

---
+++ Lab8/Lab8_Mulick.sql
@@ -0,0 +1,54 @@
+﻿Drop table if exists director_creds;
+Drop table if exists actor_creds;
+Drop table if exists directors;
+Drop table if exists actors;
+Drop table if exists movies;
+Drop table if exists people;
+
+create table people(
+  peopleID              int       primary key,
+  fName                 text      not null,
+  lName                 text      not null,
+  spouseFName           text,
+  spouseLName           text,
+  address               text      not null
+);
+
+create table directors(
+  peopleID              int       primary key references people(peopleID),
+  filmSchool            text,
+  directorsGuildDate    date,
+  favoriteLensMaker     text
+);
+
+create table actors(
+  peopleID              int       primary key references people(peopleID),
+  birthDate             date      not null,
+  hairColor             text      not null,
+  heightInches          int       not null,
+  weight                int       not null,
+  favoriteColor         text,
+  sagAnnivDate          date
+);
+
+create table movies(
+  movieID               int       primary key,
+  name                  text      not null,
+  year                  int       not null,
+  mpaaRating            text      not null,
+  domesticBoxSalesUSD   int,
+  foreignBoxSalesUSD    int,
+  dvdBlueRaySalesUSD    int
+);
+
+create table actorCredits(
+  peopleID              int       references actors(peopleID),
+  movieID               int       references movies(movieID),
+  primary key(peopleID, movieID)
+);
+
+create table directorCredits(
+  peopleID              int       references directors(peopleID),
+  movieID               int       references movies(movieID),
+  primary key(peopleID, movieID)
+);
