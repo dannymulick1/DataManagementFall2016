c18cc23e65382d91e34d19a8bd36ac4d96d09564
dannymulick1 <danny.mulick1@marist.edu>
2016-11-30 20:52:42 -0500

    Commit to add file for lab 10
    Same as title

---
+++ Lab10/Lab10_Mulick.sql
@@ -0,0 +1,42 @@
+﻿--Danny Mulick 12/1/2016 Lab 10: Stored Procedure
+--Func1on PreReqsFor(courseNum) - Returns the immediate prerequisites for the passed-in course number.
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor 
+as 
+$$
+declare
+   inputCourse int       := $1;
+     resultset REFCURSOR := $2;
+begin
+   open resultset for 
+      select prereqnum
+        from prerequisites
+       where courseNum = inputCourse;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select PreReqsFor(499, 'results');
+fetch all from results;
+
+--func1on IsPreReqFor(courseNum) - Returns the courses for which the passed-in course number is an immediate pre-requisite.
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor 
+as 
+$$
+declare
+   inputCourse int       := $1;
+     resultset REFCURSOR := $2;
+begin
+   open resultset for 
+      select courseNum
+        from Prerequisites
+       where preReqNum = inputCourse;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select IsPreReqFor(221, 'results');
+fetch all from results;
+
+--Demonstrate Jedi-level skills and write a third, recursive, function that takes a passed-in course number and generates all of its prerequisites. Uses the first two functions you wrote and recursion.
